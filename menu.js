f8156dd39aaba8c3affea6390286af30160b90e1
Ahimenes17 <86240967+Ahimenes17@users.noreply.github.com>
2024-04-17 19:46:39 +1000

    Changes in scoresScripts, mainscripts and menu files

---
+++ menu.js
@@ -0,0 +1,19 @@
+var btn_start_game = document.getElementById("button-start-game")
+var btn_scores = document.getElementById("button-scores")
+var btn_settings = document.getElementById("button-settings")
+
+btn_start_game.addEventListener("click", function () {StartGame ()})
+btn_scores.addEventListener("click", function () {ShowScores ()})
+btn_settings.addEventListener("click", function () {ShowSettings ()})
+
+function StartGame(){
+    window.location.href = "file:///home/ahimenes17/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/GitHub/Tetris_Hihiik/tetris1.html?name=desmond";
+}
+
+function ShowScores(){
+
+}
+
+function ShowSettings(){
+
+}
--- scoresScripts.js
+++ scoresScripts.js
@@ -13,9 +13,9 @@ function send_score(){
 	score.defaults({players:{}})
     .write()
     var name = document.getElementById('name');
-    var score = document.getElementById('score');
-    var level = document.getElementById('level');
-    const score_json = {"name_p": name, "score_p": score, "level_p": level}
+    var score = document.getElementById('score_p');
+    var level = document.getElementById('level_p');
+    const score_json = {"name": name, "score": score, "level": level}
     scores.get('players')
     .push(score_json)
     .write()
