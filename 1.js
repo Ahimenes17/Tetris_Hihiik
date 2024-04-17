27b1d85ccaff4a594a417ef85fcfceed35ffe961
Ahimenes17 <86240967+Ahimenes17@users.noreply.github.com>
2024-04-17 20:30:47 +1000

    Add  new important files

--- 1.js
+++
@@ -1,6 +0,0 @@
-import {JSONFilePreset} from 'lowdb/node'
-
-const scores = {players:[]}
-const db = await JSONFilePreset('scores.json', scores)
-
-await db.update(({persons }) => persons.push('Mike'))
