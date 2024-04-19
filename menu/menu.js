9b3e504ceaf9b379ff5e123fdfc4904fcc559888
Ahimenes17 <86240967+Ahimenes17@users.noreply.github.com>
2024-04-19 21:15:35 +1000

    Key back

--- menu/menu.js
+++ menu/menu.js
@@ -15,5 +15,5 @@ function ShowScores(){
 }
 
 function ShowSettings(){
-
+    window.location.href = "file:///home/ahimenes17/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/GitHub/Tetris_Hihiik/settings/settings.html"
 }
---
+++ settings/settings.js
@@ -0,0 +1,7 @@
+var btn1back = document.getElementById("btn3Back")
+
+btn1back.addEventListener("click", function () {BackMenu1 ()})
+
+function BackMenu1(){
+    window.location.href = "file:///home/ahimenes17/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/GitHub/Tetris_Hihiik/menu/menu.html"
+}
